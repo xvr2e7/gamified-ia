2243326c9298b6d285811f4d12526c3d759472c3
xvr2e7 <xvx_sh@outlook.com>
2025-07-08 17:22:43 +0200

    Update left and right gaze providers

--- Assets/Scripts/LeftEyeGazeProvider.cs
+++ Assets/Scripts/LeftEyeGazeProvider.cs
@@ -14,17 +14,24 @@ public class LeftEyeGazeProvider : MonoBehaviour, IEyeGazeProvider
     public bool TryGetGazePose(out Pose gazePose)
     {
         gazePose = default;
-        XrSingleEyeGazeDataHTC[] gazeData;
-        XR_HTC_eye_tracker.Interop.GetEyeGazeData(out gazeData);
-        var left = gazeData[(int)XrEyePositionHTC.XR_EYE_POSITION_LEFT_HTC];
-        if (!left.isValid) return false;
 
-        Vector3 worldPos = mainCamera.transform.TransformPoint(
-            left.gazePose.position.ToUnityVector());
-        Quaternion worldRot = mainCamera.transform.rotation *
-                              left.gazePose.orientation.ToUnityQuaternion();
+        try
+        {
+            XrSingleEyeGazeDataHTC[] gazeData;
+            XR_HTC_eye_tracker.Interop.GetEyeGazeData(out gazeData);
+            var leftGaze = gazeData[(int)XrEyePositionHTC.XR_EYE_POSITION_LEFT_HTC];
 
-        gazePose = new Pose(worldPos, worldRot);
-        return true;
+            if (!leftGaze.isValid) return false;
+
+            Vector3 worldPos = mainCamera.transform.TransformPoint(leftGaze.gazePose.position.ToUnityVector());
+            Quaternion worldRot = mainCamera.transform.rotation * leftGaze.gazePose.orientation.ToUnityQuaternion();
+
+            gazePose = new Pose(worldPos, worldRot);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
--- Assets/Scripts/RightEyeGazeProvider.cs
+++ Assets/Scripts/RightEyeGazeProvider.cs
@@ -14,17 +14,24 @@ public class RightEyeGazeProvider : MonoBehaviour, IEyeGazeProvider
     public bool TryGetGazePose(out Pose gazePose)
     {
         gazePose = default;
-        XrSingleEyeGazeDataHTC[] gazeData;
-        XR_HTC_eye_tracker.Interop.GetEyeGazeData(out gazeData);
-        var right = gazeData[(int)XrEyePositionHTC.XR_EYE_POSITION_RIGHT_HTC];
-        if (!right.isValid) return false;
 
-        Vector3 worldPos = mainCamera.transform.TransformPoint(
-            right.gazePose.position.ToUnityVector());
-        Quaternion worldRot = mainCamera.transform.rotation *
-                              right.gazePose.orientation.ToUnityQuaternion();
+        try
+        {
+            XrSingleEyeGazeDataHTC[] gazeData;
+            XR_HTC_eye_tracker.Interop.GetEyeGazeData(out gazeData);
+            var rightGaze = gazeData[(int)XrEyePositionHTC.XR_EYE_POSITION_RIGHT_HTC];
 
-        gazePose = new Pose(worldPos, worldRot);
-        return true;
+            if (!rightGaze.isValid) return false;
+
+            Vector3 worldPos = mainCamera.transform.TransformPoint(rightGaze.gazePose.position.ToUnityVector());
+            Quaternion worldRot = mainCamera.transform.rotation * rightGaze.gazePose.orientation.ToUnityQuaternion();
+
+            gazePose = new Pose(worldPos, worldRot);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
